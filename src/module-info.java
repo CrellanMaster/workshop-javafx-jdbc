decadb50ee2df2d31bc62e1af8be4539883f34e5
KodeMaster <devsandrounity@gmail.com>
2024-08-05 20:40:17 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,9 @@
 module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
-	
+
 	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.fxml;
+
+	exports gui;
 }
