d7e601ff88f17bdd448f9744a5241f1b210b5f43
KodeMaster <devsandrounity@gmail.com>
2024-08-08 11:02:35 -0300

    DepartmentService findAll and findById implementation

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,16 +1,20 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.EntityDao;
 import model.entities.Department;
 
 public class DepartmentService {
+
+	private EntityDao<Department> dao = DaoFactory.createDepartmentDao();
+
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Electronics"));
-		return list;
+		return dao.findAll();
+	}
+
+	public Department findById(Integer id) {
+		return dao.findById(id);
 	}
 }
