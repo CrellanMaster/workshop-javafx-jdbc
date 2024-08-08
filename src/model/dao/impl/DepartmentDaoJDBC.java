239400a239f5211f9cc5ff41dc3dd91160e87a41
KodeMaster <devsandrounity@gmail.com>
2024-08-08 10:35:51 -0300

    Department findAll implementation

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -41,8 +43,25 @@ public class DepartmentDaoJDBC implements EntityDao<Department> {
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = this.conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM department ORDER BY name");
+
+			List<Department> list = new ArrayList<>();
+			while (rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				list.add(dep);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
